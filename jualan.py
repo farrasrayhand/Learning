c7a683c7fed52692ece80e45e331c1c06f0be3b8
Muhammad Farras Rayhand <35839373+farrasrayhand@users.noreply.github.com>
2021-04-28 17:56:16 +0800

    Update jualan.py

--- jualan.py
+++ jualan.py
@@ -1,50 +1,69 @@
 import os
+import sys
+import getpass
+import pandas as pd
+import openpyxl
 
-while True:
-	saldo = 0
-	print (Mau Top UP Saldo berapa?)
-	print (1. Saldo 5000)
-	print (2. Saldo 10000)
-	print (3. Saldo 50000)
-	print (4. Saldo 100000)
-	a = int (input ('Masukkan Pilihan anda : '))
+book = openpyxl.load_workbook('data.xlsx')
+sheet = book.active
 
-	if a == '1':
-		saldo+5000
-		print ('saldo anda = 'saldo)
-		d = input('lagi? (y) : ')
-		if d == 'y':
-			continue
-			os.system('cls||clear')
-		else:
-			break
-	elif a == '2':
-		saldo+5000
-		print ('saldo anda = 'saldo)
-		d = input('lagi? (y) : ')
-		if d == 'y':
-			continue
-			os.system('cls||clear')
-		else:
-			break
-	elif a == '3':
-		saldo+5000
-		print ('saldo anda = 'saldo)
-		d = input('lagi? (y) : ')
-		if d == 'y':
-			continue
-			os.system('cls||clear')
-		else:
-			break
-	elif a == '4':
-		print (a/b)
-		d = input('lagi? (y) : ')
-		if d == 'y':
-			continue
-			os.system('cls||clear')
-		else:
-			break
-	else:
+pulsa_1 = int(sheet['E2'].value)
+data_1 = sheet['F2']
+lanjut_pulsa = "n"
+username = str(sheet['B2'].value)
+password = str(sheet['C2'].value)
+nama = str(sheet['D2'].value)
+login = "gagal"
+
+
+while login == "gagal":
+	os.system('cls||clear')
+	print("Silahkan login terlebih dahulu!")
+	login_user = input("Masukkan Username : ")
+	pass_user = getpass.getpass("Masukkan Password : ")
+	if login_user == username and pass_user == password:
+		input("Login Berhasil Selamat Datang!")
 		os.system('cls||clear')
-		print ('masukkan anda salah!')
+		login = "berhasil"
+	else:
+		input("Username atau Password Salah!")
+		os.system('cls||clear')
+
+while login == "berhasil":
+	print("Halo",nama,"! Mau Pilih yang mana?")
+	print("1. Pulsa")
+	print("2. Paket Data")
+	print("3. Cek Pulsa dan Paket Data")
+	print("4. Top UP Saldo")
+	menu = int (input ("Masukkan Pilihan anda : "))
+	if menu == 1:
+		print("Beli pulsa : ")
+		print("1.Beli pulsa Rp.5.000")
+		print("2.Beli Pulsa Rp.10.000")
+		print("3.Beli Pulsa Rp.20.000")
+		print("4.Keluar aplikasi")
+		a = int (input("Silahkan pilih pulsa yang mau di beli : "))
+		if a == 1:
+			sheet['E2'].value = pulsa_1+5000
+			book.save('data.xlsx')
+			print ("Anda berhasil membeli pulsa Rp.5000 total pulsa ", pulsa_1)
+		elif a == 2:
+			sheet['E2'].value = pulsa_1+10000
+			book.save('data.xlsx')
+			print ("Anda berhasil membeli pulsa Rp.10000 total pulsa ", pulsa_1)
+		elif a == 3:
+			sheet['E2'].value = pulsa_1+20000
+			book.save('data.xlsx')
+			print ("Anda berhasil membeli pulsa Rp.20000 total pulsa ", pulsa_1)
+		else:
+			break
+	elif menu == 2:
+		lanjut_kuota = "y"
+	elif menu == 3:
+		print("Pulsa anda : ",pulsa_1)
+		print("Paket data anda : ",data_1.value)
+		input("Press Enter to continue...")
+		os.system('cls||clear')
+	else:
+		print("Pilihan tidak tersedia!")
 		continue
